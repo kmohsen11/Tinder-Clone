ffc3ea67822f4786c2332e5f78605e80d45dbac7
Khaled Ahmed <kmhsn217@gmail.com>
2024-03-01 19:24:18 -0800

    commit changes

--- mentos/src/Header.js
+++ mentos/src/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <div>
-        <h1>ahaaa </h1>
-    </div>
+    <div className="header">  
+       
+        <h1>Header</h1>
+        <img 
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/2560px-Dunder_Mifflin%2C_Inc.svg.png" 
+        alt="Tempo" />
+        </div>
   );
 } 
 export default Header;
