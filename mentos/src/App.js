c1d76562e4c24de961717d01e8534358bb88bd2f
Khaled Ahmed <kmhsn217@gmail.com>
2024-03-01 19:37:56 -0800

    Updated navbar

--- mentos/src/App.js
+++ mentos/src/App.js
@@ -5,9 +5,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <h1>Mentos</h1>
       <Header />
-      
 
     </div>
   );
--- mentos/src/Header.js
+++ mentos/src/Header.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./Header.css";
-
+import IconButton from '@mui/material/IconButton';
 function Header() {
   return (
     <div className="header">  
-       
-        <h1>Header</h1>
+        <IconButton>
+        <h1 style={{ fontSize: '1rem', margin: 0 }}>Main page</h1>
+      </IconButton>
+
+      <IconButton>
+        <h1 style={{ fontSize: '1rem', margin: 0 }}>Score board</h1>
+      </IconButton>
+
+      <IconButton>
+        <h1 style={{ fontSize: '1rem', margin: 0 }}>Messages</h1>
+      </IconButton>
+
+      <IconButton>
+        <h1 style={{ fontSize: '1rem', margin: 0 }}>Study Sessions</h1>
+      </IconButton>
+
+      <IconButton>
+        <h1 style={{ fontSize: '1rem', margin: 0 }}>Teach</h1>
+      </IconButton>
+
+      <IconButton>
+        <h1 style={{ fontSize: '1rem', margin: 0 }}>Learn</h1>
+      </IconButton>
+
+      <IconButton>
         <img 
-        className="header__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/2560px-Dunder_Mifflin%2C_Inc.svg.png" 
-        alt="Tempo" />
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/2560px-Dunder_Mifflin%2C_Inc.svg.png" 
+          alt="Tempo" 
+          style={{ height: '50px' }} // Adjust size as needed
+        />
+      </IconButton>
+
         </div>
   );
 } 
